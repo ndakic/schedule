0a24f38b734c936772f75df8937982cdd42f1f51
Nikola Dakic <nikoladakic@uns.ac.rs>
2018-05-29 09:24:41 +0200

    tutsFix

---
+++ src/main/resources/static/guide/guide.js
@@ -0,0 +1,106 @@
+/**
+ * Created by daka on 5/29/18.
+ */
+
+
+var simpleModal = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+var accTutBtn = document.getElementById('accTutBtn');
+var decTutBtn = document.getElementById('decTutBtn');
+var guideTutBtn = document.getElementById('guideTutBtn');
+
+// Get open modal button
+
+
+// Listen for open click
+//     modalBtn.addEventListener('click', function() {
+//         openModal(simpleModal);
+//     }, false);
+
+// Listen for close click
+//    closeBtn.addEventListener('click', function() {
+//        closeModal(simpleModal);
+//   }, false);
+
+// Listen for outside click
+
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+accTutBtn.addEventListener('click', accTutFun);
+decTutBtn.addEventListener('click', decTutFun);
+guideTutBtn.addEventListener('click', guideTutFun);
+
+if(localStorage.getItem("tutorijal") == null) {
+    window.onload = function() {
+        openModal();
+    };
+}
+
+
+function decTutFun() {
+    localStorage.setItem("tutorijal", false);
+    closeModal();
+}
+
+function accTutFun(){
+    localStorage.setItem("tutorijal", "hint");
+    javascript:introJs().addHints();
+    closeModal();
+}
+
+
+function guideTutFun(){
+    localStorage.setItem("tutorijal", "guide");
+    introJs().setOption('doneLabel', 'Next page').start('weeks').oncomplete(function() {
+        window.location.href = '/#!/settings?multipage=true';
+    });
+    closeModal();
+}
+
+function settingsGuide() {
+    if(localStorage.getItem("tutorijal") == "guide") {
+        introJs().setOption('doneLabel', 'Next page').start('settings').oncomplete(function() {
+            window.location.href = '/#!/settings?multipage=true';
+        });
+    }
+}
+
+// Function to open modal
+function openModal(){
+    simpleModal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+    localStorage.setItem("tutorijal", false);
+    simpleModal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+    if(e.target == simpleModal){
+        localStorage.setItem("tutorijal", false);
+        simpleModal.style.display = 'none';
+    }
+}
+
+
+if (RegExp('multipage', 'gi').test(window.location.search)) {
+    introJs().start();
+}
+
+
+
+//===============================================================
+
+
+function startTour() {
+    var tour = introJs()
+    tour.setOption('tooltipPosition', 'auto');
+    tour.setOption('positionPrecedence', ['left', 'right', 'top', 'bottom']);
+    tour.start();
+}
