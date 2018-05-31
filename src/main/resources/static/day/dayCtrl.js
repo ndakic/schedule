9d1f2833696b204b3a1c7b7c46da3c54ecd02ade
Nikola Dakic <nikoladakic@uns.ac.rs>
2018-06-01 00:43:54 +0200

    moveCourses

--- src/main/resources/static/day/dayCtrl.js
+++ src/main/resources/static/day/dayCtrl.js
@@ -8,12 +8,14 @@
         .module('HCIApp')
         .controller('dayCtrl', dayCtrl);
 
-        dayCtrl.$inject = ['$scope', '$http','$location', 'Alertify', 'entity'];
+        dayCtrl.$inject = ['$scope', '$http','$location', 'Alertify', 'entity', 'Day'];
 
-         function dayCtrl($scope, $http, $location, Alertify, entity) {
+         function dayCtrl($scope, $http, $location, Alertify, entity, Day) {
             var vm = this;
             vm.checkSettings = checkSettings;
             $scope.entity = entity;
+
+            // $scope.day = Day.get({id : $scope.entity.id}).$promise;
 
             $scope.selected = {};
             $scope.courseList = [];
@@ -22,7 +24,16 @@
             $scope.lists = entity.timePeriodList;
             $scope.autocompleteOptions = ["ON", "OFF"];
             $scope.autocomplete = "ON";
+            $scope.moved = false;
+            $scope.insertPosition = [];
 
+            var loadDay = function () {
+                 var promise = $http.get("/api/day/" + $scope.entity.id);
+                 promise.then(function (response) {
+                     $scope.lists = response.data.timePeriodList;
+                     //console.log("test: ", $scope.test.timePeriodList);
+                 });
+             };
 
             function saveDay() {
                 $scope.data = {
@@ -62,40 +73,148 @@
              }
 
             $scope.logEvent = function(message, course) {
+
                  $scope.draggedCourse = course;
+
+
+                 if(message == 'Started1')
+                     $scope.moved = true;
+
+                console.log("moved: ", $scope.moved);
              };
 
-            $scope.logListEvent = function(action, index, external, type, classroom, time, order) {
+            $scope.logListEvent = function(action, index, external, type, classroom, timePeriod, order) {
 
-                 var calc_addition = ($scope.draggedCourse.duration - 15)/15;
-                 var count = 0;
 
-                 if($scope.autocomplete === "ON"){
-                     out: for(var time in $scope.lists){
-                         if($scope.lists[time]["ordertime"] > order && $scope.lists[time]["ordertime"] <= order + calc_addition){
-                             console.log($scope.lists[time]["ordertime"]);
-                             for(var room in $scope.lists[time]["classrooms"]){
-                                 if($scope.lists[time]["classrooms"][room]["classroom"] == classroom){
-                                     console.log("room: ", $scope.lists[time]["classrooms"][room]);
+                $scope.already = true;
 
-                                     if($scope.lists[time]["classrooms"][room]["course"].length == 0){
-                                         if(count != 2)
-                                             $scope.lists[time]["classrooms"][room]["course"].push($scope.draggedCourse);
+                if($scope.moved == false)
+                    if(checkIfAlreadyAdded($scope.draggedCourse))
+                        $scope.already = false;
 
-                                         count++;
-                                         if(count == 2)
-                                             calc_addition++;
-                                     }else{
-                                         console.log("BREAK: ", $scope.lists[time]["classrooms"][room]["course"]);
-                                         break out;
-                                     }
+
+
+                if(classroom != null && timePeriod != null){
+
+                     var calc_addition = ($scope.draggedCourse.duration - 15)/15;
+                     var count = 0;
+
+                     var status = true;
+
+                    $scope.insertPosition.push(timePeriod);
+
+                     if($scope.autocomplete === "ON"){
+                         out: for(var time in $scope.lists){
+                             if($scope.lists[time]["ordertime"] > order && $scope.lists[time]["ordertime"] <= order + calc_addition){
+                                 for(var room in $scope.lists[time]["classrooms"]){
+                                     if($scope.lists[time]["classrooms"][room]["classroom"] == classroom){
+                                         if($scope.lists[time]["classrooms"][room]["course"].length == 0){
+                                             if(count != 2){
+                                                 $scope.lists[time]["classrooms"][room]["course"].push($scope.draggedCourse);
+                                                 $scope.insertPosition.push($scope.lists[time]);
+                                                 $scope.insertClassroom = classroom;
+                                             }
+
+
+                                             count++;
+                                             if(count == 2)
+                                                 calc_addition++;
+                                         }else{
+                                             console.log('else: ', $scope.lists[time]["classrooms"][room]["course"]);
+                                             Alertify.error("Course can't be dragged here. Try again.");
+                                             status = false;
+                                             break out;
+                                         }
+                                     };
                                  };
                              };
                          };
                      };
-                 };
 
-                 saveDay();
+
+                    if($scope.moved == true){
+
+                        for(var pos in $scope.insertPosition)
+                            console.log($scope.insertPosition[pos]);
+
+                        for(var time in $scope.lists){
+                            for(var room in $scope.lists[time]["classrooms"]){
+                                if(checkTimePeriodsAndClassrooms($scope.lists[time], $scope.lists[time]["classrooms"][room]))
+                                    if($scope.lists[time]["classrooms"][room]["course"].length == 1)
+                                        if($scope.lists[time]["classrooms"][room]["course"][0].id == $scope.draggedCourse.id){
+                                            $scope.lists[time]["classrooms"][room]["course"] = [];
+                                            console.log("DELETED!");
+                                        }
+                            }
+                        }
+
+                        $scope.moved = false;
+                        $scope.insertPosition = [];
+                    }
+
+                    if(status && $scope.already)
+                        saveDay();
+                    else
+                        loadDay();
+
+
+                }else{
+                    console.log("del dragged:", $scope.draggedCourse);
+                    saveDay();
+                }
+
+             };
+
+            // koje ne treba obrisati
+             function checkTimePeriodsAndClassrooms(period, room) {
+
+                 var status = true;
+
+                 for(var per in $scope.insertPosition)
+                     if($scope.insertPosition[per].time == period.time && room.classroom == $scope.insertClassroom){
+                         status = false;
+                         console.log("false: ", $scope.insertPosition[per].time, room.classroom);
+                     }
+
+                 return status;
+                 
+             }
+
+             function checkIfAlreadyAdded(course) {
+
+                 var status = false;
+
+                 console.log("check:", course.id);
+
+                 var allowed = course.duration / 15;
+
+                 var count = 0;
+
+                 for(var time in $scope.lists){
+                     for(var room in $scope.lists[time]["classrooms"])
+                         for(var cour in $scope.lists[time]["classrooms"][room].course)
+                             if($scope.lists[time]["classrooms"][room].course[cour].id == course.id)
+                                 count++;
+                 }
+
+                 console.log("count: ", count);
+                 console.log("allowed: ", allowed);
+
+                 if(count > 1){
+                     status = true;
+                     Alertify.error("Course already added!");
+
+                 }
+
+
+                 return status;
+
+             }
+
+             $scope.dropCallback = function(index, item, external, type) {
+                 $scope.logListEvent('dropped at', index, external, type);
+                 // Return false here to cancel drop. Return true if you insert the item yourself.
+                 return item;
              };
 
              var loadClassrooms = function () {
@@ -114,9 +233,9 @@
                  for(var room in $scope.classroomSettings){
                      if($scope.classroomSettings[room]["id"] == classrooom){
                          return $scope.classroomSettings[room]["status"];
-                     }
-                 }
-             }
+                     };
+                 };
+             };
 
         };
 }(angular));
